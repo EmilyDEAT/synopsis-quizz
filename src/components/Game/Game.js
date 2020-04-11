87b868545ba777764aa5bb08c2085bcd1bfa37ff
Emily Déat <emily.deat@gmail.com>
2020-04-11 16:53:12 +0200

    Add div synopsis and game page

---
+++ src/components/Game/Game.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import DisplaySynopsis from './DisplaySynopsis'
+
+import './Game.css'
+
+const Game = () => {
+  return (
+    <div className='game-page'>
+      <DisplaySynopsis />
+      
+    </div>
+  )
+}
+
+export default Game
