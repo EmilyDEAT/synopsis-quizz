0bf498518ae5394072056eb3e307171348f80b5b
Emily Déat <emily.deat@gmail.com>
2020-04-11 12:41:59 +0200

    Add button play on home page

---
+++ src/components/ButtonPlay.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './ButtonPlay.css'
+
+const ButtonPlay = () => {
+  return(
+    <button className="button-play" onClick="">
+      JOUER
+    </button>
+  )
+}
+
+export default ButtonPlay
